ae3adfd4016597420b52a0a7af82b2a5c98dc0ed
lxy <2031415307@qq.com>
2019-09-28 18:50:12 +0800

    first

---
+++ spring-boot-demo-hello/src/main/java/com/lxyy/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.lxyy.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @RestController： 是@ResponseBody和@Controller的组合注解
+ * @RequestMapping： 配置url映射，可以作用在控制器的某个方法上，也可以作用在此控制器类上
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping("/sayHello")
+    public String sayHello() {
+        return "helloWorld";
+    }
+}
